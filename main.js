b2a4d71dc1c593ace3c5162f0000443cadb3bd43
ViktoriaMagina <victoria.magina@yandex.ru>
2022-04-06 21:47:45 +0300

    добавлены новые вопросы и комментарии

--- main.js
+++ main.js
@@ -1,48 +1,57 @@
-const headerContainer = document.querySelector("#header");
-const listContainer = document.querySelector("#list");
-const submitBtn = document.querySelector("#submit");
+const headerContainer = document.querySelector("#header"); //контейнер для заголовка
+const listContainer = document.querySelector("#list"); //контейнер для вариантов ответа
+const submitBtn = document.querySelector("#submit"); //кпопка отправки ответа
 
 let score = 0; //количество очков
 let questionIndex = 0; //индекс текущего вопроса
 
+
+// массив с объектами, содержащими вопросы, варианты ответов и номер правильного ответа
 const questions = [
 	{
-		question: "Какой язык работает в браузере?",
-		answers: ["Java", "C", "Python", "JavaScript"],
-		correct: 4,
-	},
-	{
-		question: "Что означает CSS?",
-		answers: [
-			"Central Style Sheets",
-			"Cascading Style Sheets",
-			"Cascading Simple Sheets",
-			"Cars SUVs Sailboats",
-		],
-		correct: 2,
-	},
-	{
-		question: "Что означает HTML?",
-		answers: [
-			"Hypertext Markup Language",
-			"Hypertext Markdown Language",
-			"Hyperloop Machine Language",
-			"Helicopters Terminals Motorboats Lamborginis",
-		],
+		question: "Вставьте пропущенное слово: «Мы есть то что мы… из дня в день.» Аристотель",
+		answers: ["Делаем", "Едим", "Думаем", "Чувствуем"],
 		correct: 1,
 	},
 	{
-		question: "В каком году был создан JavaScript?",
-		answers: ["1996", "1995", "1994", "все ответы неверные"],
-		correct: 2,
+		question: "Что такое Атихифобия?",
+		answers: [
+			"Страх приёма лекарственных средств",
+			"Боязнь пауков",
+			"Страх перед едой",
+			"Страх совершить ошибку",
+		],
+		correct: 4,
+	},
+	{
+		question: "У какого персонажа Симпсонов пять пальцев?",
+		answers: [
+			"Гомер Симпсон",
+			"Барт Симпсон",
+			"Бог",
+			"Клоун",
+		],
+		correct: 3,
+	},
+	{
+		question: "В августе 2010 образовалась самая длинная пробка на дороге, продолжительностью 10 дней и протяжённостью в 100 км. Где это произошло?",
+		answers: ["Пекин", "Нью-Йорк", "Москва", "Шанхай"],
+		correct: 1,
+	},
+	{
+		question: "Какой срок годности ГОСТ предписывал советскому мороженому?",
+		answers: ["5 дней", "Месяц", "Неделя", "14 дней"],
+		correct: 3,
 	},
 ];
 
+//функция отчистки html разметки
 const clearPage = () => {
 	headerContainer.innerHTML = "";
 	listContainer.innerHTML = "";
 }
 
+//функция, выполняющая рендеринг текущего вопроса
 const showQuestion = () => {
 	let inputValue = 1
 	const headerHtml = `<h2 class="title">%title%</h2>`
@@ -63,6 +72,7 @@ const showQuestion = () => {
 	}
 }
 
+//функция, показывающая результат игры
 const showResult =() => {
 	const headerHtml = `<h2 class="title">Викторина окончена!</h2>`
 	headerContainer.innerHTML = headerHtml
@@ -75,6 +85,7 @@ const showResult =() => {
 	submitBtn.innerText = "Пройти заново"
 }
 
+//функция, проверяющая правильность ответа
 const checkAnswer = () => {
 	if (questionIndex === parseInt(questions.length)){
 		location.reload() 
@@ -96,9 +107,10 @@ const checkAnswer = () => {
 		showResult()
 	}
 }
-
+//передача функции checkAnswer по клику на submitBtn
 submitBtn.onclick = checkAnswer;
 
+//первоначальный рендеринг вопросов
 showQuestion()  
 
 
